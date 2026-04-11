c7189c9559449dfdb216036ffc1fc400d6d63a86
ARJ2211 <aayushrj22@gmail.com>
2026-04-10 21:32:02 -0400

    Added the submit method

--- collect.go
+++ collect.go
@@ -13,8 +13,28 @@ type TypedGrove[T any] struct {
 	results []T        // the results of the function
 }
 
+// this function will call grove.Go which will
+// wrap the actual function whose value we need
+// and append it to the results of TypedGrove.
 func (tg *TypedGrove[T]) Submit(
 	name string,
 	fn func(ctx context.Context) (T, error),
 ) {
+	tg.grove.Go(name, func(ctx context.Context) error {
+		var result T
+		var err error
+
+		// call the function
+		result, err = fn(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		tg.mu.Lock()
+		defer tg.mu.Unlock()
+		tg.results = append(tg.results, result)
+
+		return nil
+	})
 }
