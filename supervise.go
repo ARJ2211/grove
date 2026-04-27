9d31b7fa818ee3683b33e169f9f216a761d60e52
ARJ2211 <aayushrj22@gmail.com>
2026-04-27 17:26:25 -0400

    function for launch task

--- supervise.go
+++ supervise.go
@@ -2,7 +2,10 @@ package grove
 
 import (
 	"context"
+	"fmt"
 	"time"
+
+	"github.com/ARJ2211/grove/internal"
 )
 
 // task structure for the supervisor
@@ -39,4 +42,32 @@ func Supervise(
 	fn func(g *Grove) error,
 ) error {
 	return ErrNotImplemented
+}
+
+// launch a task into the supervisor
+func launchTask(
+	ctx context.Context,
+	task task,
+	resChan chan<- taskResult,
+) {
+	go func() {
+		// run the function under a Capture Panic wrapper
+		err := internal.CapturePanic(func() error { return task.fn(ctx) })
+
+		var tr taskResult
+		if err != nil {
+			tr = taskResult{
+				task: task,
+				err:  fmt.Errorf("task [%s] - %w", task.name, err),
+			}
+		} else {
+			tr = taskResult{
+				task: task,
+				err:  nil,
+			}
+		}
+
+		// send the task result over the channel
+		resChan <- tr
+	}()
 }
